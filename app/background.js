7a5a91c6f380f910e8adc4e7ed3ae43ada0ff668
mosugi <mosuging@gmail.com>
2021-02-13 15:22:35 +0900

    send active tab

--- app/background.js
+++ app/background.js
@@ -1,6 +1,15 @@
 chrome.browserAction.onClicked.addListener(() => {
-    chrome.runtime.sendNativeMessage("com.mosugi.app.mshare", {});
-    // chrome.tabs.getSelected(null, function (tab) {
-    //   console.log(tab.url);
-    // });
+    //TODO remove
+    let url = "https://yahoo.co.jp";
+    chrome.tabs.query({active: true, currentWindow: true}, (e) => {
+        url = e[0].url;
+        console.log(url);
+    });
+    chrome.runtime.sendNativeMessage("com.mosugi.app.mshare", {service: "notes", url: url}, function (response) {
+        if (chrome.runtime.lastError) {
+            console.log("ERROR: " + chrome.runtime.lastError.message);
+        } else {
+            console.log("Messaging host say: ", response);
+        }
+    })
 });
