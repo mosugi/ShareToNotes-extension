5ae12230175093b9a3346bfe958fd1ad6cf4aca7
mosugi <mosuging@gmail.com>
2021-02-13 18:24:31 +0900

    Add share url to notes

--- app/background.js
+++ app/background.js
@@ -1,15 +1,12 @@
 chrome.browserAction.onClicked.addListener(() => {
-    //TODO remove
-    let url = "https://yahoo.co.jp";
     chrome.tabs.query({active: true, currentWindow: true}, (e) => {
-        url = e[0].url;
-        console.log(url);
+        const url = e[0].url;
+        chrome.runtime.sendNativeMessage("com.mosugi.app.mshare", {service: "notes", text: url}, function (response) {
+            if (chrome.runtime.lastError) {
+                console.log("ERROR: " + chrome.runtime.lastError.message);
+            } else {
+                console.log("Messaging host say: ", response);
+            }
+        })
     });
-    chrome.runtime.sendNativeMessage("com.mosugi.app.mshare", {service: "notes", url: url}, function (response) {
-        if (chrome.runtime.lastError) {
-            console.log("ERROR: " + chrome.runtime.lastError.message);
-        } else {
-            console.log("Messaging host say: ", response);
-        }
-    })
 });
