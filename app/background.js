1d99fcb3bc7ce6e76c4b72473c113aaff3f9cd12
mosugi <mosuging@gmail.com>
2021-02-27 12:09:24 +0900

    Open how to install mshare

--- app/background.js
+++ app/background.js
@@ -1,12 +1,22 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.create({
+    url: "https://mosugi.github.io/ShareToNotes-extension/",
+  });
+});
+
 chrome.browserAction.onClicked.addListener(() => {
-    chrome.tabs.query({active: true, currentWindow: true}, (e) => {
-        const url = e[0].url;
-        chrome.runtime.sendNativeMessage("com.mosugi.app.mshare", {service: "notes", text: url}, function (response) {
-            if (chrome.runtime.lastError) {
-                console.log("ERROR: " + chrome.runtime.lastError.message);
-            } else {
-                console.log("Messaging host say: ", response);
-            }
-        })
-    });
+  chrome.tabs.query({ active: true, currentWindow: true }, (e) => {
+    const url = e[0].url;
+    chrome.runtime.sendNativeMessage(
+      "com.mosugi.app.mshare",
+      { service: "notes", text: url },
+      function (response) {
+        if (chrome.runtime.lastError) {
+          console.log("ERROR: " + chrome.runtime.lastError.message);
+        } else {
+          console.log("Messaging host say: ", response);
+        }
+      }
+    );
+  });
 });
